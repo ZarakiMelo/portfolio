972ed4ebcf3f071ef7fa525d2884c5d0dd27bc46
SamyC11 <chahbisamy4@gmail.com>
2024-02-01 18:52:18 +0100

    HomePicture&TansIDEVideo

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ export const collections = {
 			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
+			video: z.string(),
+			videoType: z.string()
 		}),
 	}),
 };
